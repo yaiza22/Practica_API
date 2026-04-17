a324f58d6aa30413fa24d00fcb27537b256a20f5
yaiza22 <yaizas719@gmail.com>
2026-04-17 09:40:47 -0500

    Segundo commit, .env por error

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 const express = require('express')
+process.loadEnvFile()
+
+const app = express()
+
+const PORT = process.env.PORT
+
+app.get('/', (req, res) => {
+    res.send("Hola mundo")
+})
+
+app.listen(PORT, () => {
+    console.log('Corriendo')
+})
